ec0492efaed23428f834b5c48cf60c72b3e8bb56
蔭山忠臣 <k.tadaomi@gmail.com>
2022-12-29 16:49:08 +0100

    set up vue 2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -10,9 +6,25 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <HelloWorld :msg="msg" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      msg: 'Hello + Vue 2 :)'
+    }
+  },
+  components: {
+    HelloWorld
+  }
+})
+</script>
 
 <style scoped>
 .logo {
