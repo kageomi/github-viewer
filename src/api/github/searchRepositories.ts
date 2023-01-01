793ac7ba14514be4fd502e7dbb0832427631cbf9
蔭山忠臣 <k.tadaomi@gmail.com>
2023-01-01 22:40:58 +0100

    add language option

--- src/api/github/searchRepositories.ts
+++ src/api/github/searchRepositories.ts
@@ -16,9 +16,10 @@ interface SearchRepositoriesRequest {
 
 const searchRepositories =
   (octokit: Octokit) =>
-  async ({ signal, ...props }: SearchRepositoriesRequest) => {
+  async ({ signal, q, language, ...props }: SearchRepositoriesRequest) => {
     return await octokit.request('GET /search/repositories', {
       ...props,
+      q: language ? `${q}+language:${language}` : q,
       per_page: props.perPage,
       request: {
         signal
