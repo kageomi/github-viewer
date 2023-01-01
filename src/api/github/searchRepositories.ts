bb62d9886c9029a8f98ad2b050a2e1aa54fc422c
蔭山忠臣 <k.tadaomi@gmail.com>
2023-01-01 23:33:40 +0100

    delete unnecessary comments

--- src/api/github/searchRepositories.ts
+++ src/api/github/searchRepositories.ts
@@ -1,5 +1,4 @@
 import { Octokit } from 'octokit'
-import { Repository } from '../../types/Repository'
 
 interface SearchRepositoriesRequest {
   q: string
@@ -10,9 +9,6 @@ interface SearchRepositoriesRequest {
   page?: number
   signal?: AbortSignal
 }
-
-// type SearchRepositoriesResult = OctokitResponse<'GET /search/repositories'>
-// type SearchRepositoriesResult = ReturnType<typeof Octokit.request>
 
 const searchRepositories =
   (octokit: Octokit) =>
